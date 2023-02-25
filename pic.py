0e458192ee56fdfa32f7fc6cee4ae1323ccb7b29
s22023 <s22023@std.it-college.ac.jp>
2023-02-25 21:11:17 +0900

    dowload

--- pic.py
+++ pic.py
@@ -3,3 +3,20 @@ import random
 import shutil
 import bs4
 import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+def image(data):
+    Res = requests.get("https://www.google.com/search?hl=jp&q=" + data + "&btnG=Google+Search&tbs=0&safe=off&tbm=isch")
+     Html = Res.text
+    Soup = bs4.BeautifulSoup(Html,'lxml')
+    links = Soup.find_all("img")
+    link = random.choice(links).get("src")
+    return link
+def download_img(url, file_name):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(file_name+".png", 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+            def code():
+     
