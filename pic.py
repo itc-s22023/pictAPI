1147d242a0916774e468cbed2d881c858b08fa59
s22023 <s22023@std.it-college.ac.jp>
2023-02-25 21:20:16 +0900

    api終

--- pic.py
+++ pic.py
@@ -3,11 +3,10 @@ import random
 import shutil
 import bs4
 import ssl
-
 ssl._create_default_https_context = ssl._create_unverified_context
 def image(data):
     Res = requests.get("https://www.google.com/search?hl=jp&q=" + data + "&btnG=Google+Search&tbs=0&safe=off&tbm=isch")
-     Html = Res.text
+    Html = Res.text
     Soup = bs4.BeautifulSoup(Html,'lxml')
     links = Soup.find_all("img")
     link = random.choice(links).get("src")
@@ -18,5 +17,15 @@ def download_img(url, file_name):
         with open(file_name+".png", 'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, f)
-            def code():
-     
+def code():
+    code = ""
+    for i in range(10):
+        code += random.choice("images")
+    return code
+while True:
+    num = input("何枚保存しますか？:")
+    data = input("検索ワード:")
+    for _ in range(int(num)):
+        link = image(data)
+        download_img(link, code())
+    print("完了")
